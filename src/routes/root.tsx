66a5859e825da9c9ef2deee65e888500a8e39dc7
김정연 <jy.keem2468@gmail.com>
2023-08-01 18:59:09 +0900

    [ADD]axios 추가

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,47 +1,12 @@
 import React, { ReactElement } from 'react';
 
-interface ContactItemProps {
-  id: number;
-  name: string;
-}
-
-function ContactItem({ id, name }: ContactItemProps): ReactElement {
-  return (
-    <li>
-      <a href={`/contacts/${id}`}>{name}</a>
-    </li>
-  );
-}
-
 export default function Root(): ReactElement {
   return (
-    <>
-      <div id="sidebar">
-        <h1>React Router Contacts</h1>
-        <div>
-          <form id="search-form" role="search">
-            <input
-              id="q"
-              aria-label="Search contacts"
-              placeholder="Search"
-              type="search"
-              name="q"
-            />
-            <div id="search-spinner" aria-hidden />
-            <div className="sr-only" aria-live="polite" />
-          </form>
-          <form method="post">
-            <button type="submit">New</button>
-          </form>
-        </div>
-        <nav>
-          <ul>
-            <ContactItem id={1} name="Your Name" />
-            <ContactItem id={2} name="Your Friend" />
-          </ul>
-        </nav>
+    <div id="sidebar">
+      <h1>Komatzip</h1>
+      <div>
+        <p>테스트</p>
       </div>
-      <div id="detail" />
-    </>
+    </div>
   );
 }
