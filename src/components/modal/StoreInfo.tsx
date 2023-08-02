c29798160b2af74e625c5774e74d39a33f6a66ac
김정연 <jy.keem2468@gmail.com>
2023-08-02 13:59:29 +0900

    [PROG]가게 정보 타입 추가

--- src/components/modal/StoreInfo.tsx
+++ src/components/modal/StoreInfo.tsx
@@ -1,6 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+interface StoreInfoProps {
+  uuid: string;
+  name: string;
+  category: number;
+  description: string;
+  location: string;
+  coordinates: [number, number];
+  representImage: string;
+  tags: string[];
+  startTime: string;
+  endTime: string;
+  storeReviews: StoreReview[];
+  reviewCount: number;
+  likeCount: number;
+  iLike: boolean;
+}
+
+interface StoreReview {
+  uuid: string;
+  user: string;
+  review: string;
+}
+
 const StoreInfo = () => {
   return <div>특정 가게 정보를 가져와 모달로 구현한 컴포넌트입니다.</div>;
 };
