85a35fa6986d1fe15df60c526b86b704f95b16b0
김정연 <jy.keem2468@gmail.com>
2023-08-02 13:43:52 +0900

    [DEL]airbnb eslint 옵션 주석 처리

---
+++ src/components/modal/StoreInfo.tsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const StoreInfo = () => {
+  return <div>특정 가게 정보를 가져와 모달로 구현한 컴포넌트입니다.</div>;
+};
+
+export default StoreInfo;
