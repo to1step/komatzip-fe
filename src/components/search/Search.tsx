3ccfabcebaa337b24c709299296b626297471dda
김정연 <jy.keem2468@gmail.com>
2023-08-04 19:05:10 +0900

    [CHORE]검색창 테일윈드 적용

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 
 const Search = () => {
   return (
-    <div>
+    <div className="flex">
       <input
         id="searchInput"
         type="text"
         placeholder="목적지 키워드나 지명을 검색해보세요"
+        className="flex-shrink-0 w-558 h-49 p-4"
       />
-      <button type="submit">검색</button>
+      <button type="submit" className="flex-shrink-0 w-94 h-49 p-4">
+        검색
+      </button>
       <p>
-        <button type="button">내 위치로 찾기</button>
+        <button type="button" className="flex-shrink-0 w-94 h-49 p-4">
+          내 위치로 찾기
+        </button>
       </p>
     </div>
   );
