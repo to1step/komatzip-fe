673b9c849eda5de0115c042b036f02f3d16b69e7
김정연 <jy.keem2468@gmail.com>
2023-08-04 20:24:25 +0900

    [CHORE]검색창 컴포넌트 input, button 작게 디자인 중

---
+++ src/components/Commons/InputBox.tsx
@@ -0,0 +1,44 @@
+import React, { useState, ChangeEvent } from 'react';
+
+type Props = {
+  name: string;
+  value: string;
+  type?: string;
+  onClick?: () => void;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (event: React.ChangeEvent<HTMLInputElement>) => void; // 입력창에 focus가 되지 않았을 때
+};
+
+const InputBox = (props: Props) => {
+  const [inputValue, setInputValue] = useState('');
+  const [validationError, setValidationError] = useState('');
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!inputValue.trim()) {
+      setValidationError('목적지나 지명이 입력되지 않았어요!');
+      return;
+    }
+
+    console.log('입력된 검색어:', inputValue);
+    setInputValue('');
+    setValidationError('');
+  };
+
+  return (
+    <div>
+      <input
+        name={props.name}
+        type={props.type || 'text'}
+        onClick={props.onClick}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+        value={props.value}
+        placeholder="목적지나 지명을 입력하세요."
+      />
+    </div>
+  );
+};
+
+export default InputBox;
---
+++ src/components/Rank/Topstore.tsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+// import StoreInfo from '../modal/StoreInfo';
+
+interface StoreData {
+  uuid: string;
+  name: string;
+  category: number;
+  description: string;
+  location: string;
+  coordinates: number[];
+  representImage: string;
+  tags: string[];
+  startTime: string;
+  endTime: string;
+}
+
+const Topstore = () => {
+  const [topStore, setTopStore] = useState<StoreData | null>(null);
+  const [isModelOpen, setIsModalOpen] = useState(false);
+  const { region } = useParams<{ region: string }>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // const response = await axios.get<StoreData[]>(
+        //   `https://api.to1step.shop/v1/rank?type=store&region=${region}`,
+        // );
+        const response = await axios.get<StoreData[]>(
+          `https://api.to1step.shop/v1/rank?type=store&region=서울시%20송파구`,
+        );
+        const data: StoreData[] = response.data;
+        console.log(data);
+        setTopStore(response.data[0]); // 순위 1등 매장 정보 저장
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          console.log('API 호출 중 에러 발생:', error);
+        }
+      }
+    };
+
+    fetchData();
+  }, [region]);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className="p-4">
+      {topStore ? (
+        <div>
+          <h1 className="text-3xl font-bold mb-2">이번주 Top 매장</h1>
+          <div>
+            <button onClick={handleOpenModal}>
+              <h2 className="text-lg font-bold">{topStore.name}</h2>
+              <p className="text-sm">{topStore.description}</p>
+              <p className="text-sm">{topStore.location}</p>
+
+              <p>UUID: {topStore.uuid}</p>
+              <p>Category: {topStore.category}</p>
+              <p>Coordinates: {topStore.coordinates.join(', ')}</p>
+              <img
+                src={topStore.representImage}
+                alt={topStore.name}
+                className="w-48 h-auto mt-4"
+              />
+              <p>Start Time: {topStore.startTime}</p>
+              <p>End Time: {topStore.endTime}</p>
+
+              <div>
+                <h3 className="text-lg font-bold">Tags</h3>
+                <ul>
+                  {topStore.tags.map((tags) => (
+                    <li key={tags} className="text-sm">
+                      {topStore.tags}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <button onClick={handleCloseModal}>닫기</button>
+            </button>
+          </div>
+        </div>
+      ) : (
+        <p>데이터를 불러오는 중입니다...</p>
+      )}
+
+      {isModelOpen && (
+        <div className="modal">
+          {/* 모달추가 */}
+          <button onClick={handleCloseModal}>닫기</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Topstore;
