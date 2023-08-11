c45b34edb29ab6edbf5a197b1cc93fa1a3f1f88b
김정연 <jy.keem2468@gmail.com>
2023-08-11 23:20:38 +0900

    [IMPROVE]store에 위치 업데이트하는 locationAction을 redux에서 toolkit 형태로 마이그레이션

--- src/redux/locationActions.ts
+++ src/redux/locationActions.ts
@@ -1,7 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = ''; // 초기 값 설정
+
 // 현재 위치 주소 정보를 받아 store에 위치 업데이트
-export const updateLocation = (location: string) => {
-  return {
-    type: 'UPDATE_LOCATION',
-    payload: location,
-  };
-};
+const updateLocation = createSlice({
+  name: 'location',
+  initialState : initialState,
+  reducers: {
+    updateLocation: (state, action) => {
+      return action.payload;
+    }
+  }
+})
+
+export updateLocation;
