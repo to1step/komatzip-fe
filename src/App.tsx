dcf9d0bbedd2d21df56ca9f6bd6d8823722ecec2
Hoseong On <ohs8283@naver.com>
2023-08-12 06:59:35 +0900

    [CHORE] import  mappage 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import './App.css';
 import { User } from '@to1step/propose-backend';
 import { Route, Routes, Link } from 'react-router-dom';
 import Post from './pages/post/Post';
 import Topstore from './components/rank/Topstore';
-import Search from './components/search/Search';
-import KakaoMap from './components/KakaoMap';
+import Search from './components/Search/Search';
+import MapPage from './pages/MapPage';
 function App() {
   return (
     <>
-      <Link to="/">진짜 한국</Link>
+      <Link to="/" className="text-3xl font-bold">
+        진짜 한국
+      </Link>
       <Search />
       <Routes>
         <Route path="/" element={<Topstore />} />
         <Route path="/post" element={<Post />} />
-        <Route path="/mappage" element={<KakaoMap />} />
+        <Route path="/mappage" element={<MapPage />} />
       </Routes>
     </>
   );
--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -4,7 +4,6 @@ import KakaoMap from '../components/KakaoMap';
 const MapPage: React.FC = () => {
   return (
     <div>
-      <h1>카카오맵을 사용하는 페이지</h1>
       <KakaoMap />
     </div>
   );
