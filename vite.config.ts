e56aa250b84d0eee1c95a1c97d58ecc94d509ab4
Hoseong On <ohs8283@naver.com>
2023-08-12 06:44:45 +0900

    [CHORE] normalize.css 재설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      // 다른 설정은 생략하고 normalize.css만 추가
+      additionalData: `@import "normalize.css";`,
+    },
+  },
 });
