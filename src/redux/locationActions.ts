1f2dffaed9346cbcbb6540bd33439af7f6002e87
김정연 <jy.keem2468@gmail.com>
2023-08-11 23:36:17 +0900

    [IMPROVE]updateLocation이 일반 변수로 export하던 것을 action 생성자로 바꿈,reducer를 default export로 변경

--- src/redux/locationActions.ts
+++ src/redux/locationActions.ts
@@ -1,16 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-const initialState = ''; // 초기 값 설정
+import { createSlice } from '@reduxjs/toolkit';
 
 // 현재 위치 주소 정보를 받아 store에 위치 업데이트
-const updateLocation = createSlice({
+const initialState = ''; // 초기 값 설정
+
+const locationSlice = createSlice({
   name: 'location',
-  initialState : initialState,
+  initialState: initialState,
   reducers: {
     updateLocation: (state, action) => {
       return action.payload;
-    }
-  }
-})
+    },
+  },
+});
 
-export updateLocation;
+export const { updateLocation } = locationSlice.actions;
+export default locationSlice.reducer;
