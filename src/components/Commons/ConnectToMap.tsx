934c702c34aa5916fb6a6de65d1d21c5544f3214
김정연 <jy.keem2468@gmail.com>
2023-08-17 22:07:45 +0900

    [CHORE]검색부분 css 적용 및 로고,파비콘 변경

--- src/components/Commons/ConnectToMap.tsx
+++ src/components/Commons/ConnectToMap.tsx
@@ -1,6 +1,6 @@
 const ConnectToMap = () => {
   return (
-    <button className="w-[675px] m-0 text-left text-sm bg-transparent border-none text-black hover:text-gray-500">
+    <button className="text-sm bg-transparent text-black hover:text-gray-500 hover:border-transparent focus:outline-none">
       내 위치로 찾기
     </button>
   );
