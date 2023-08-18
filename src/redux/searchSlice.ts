cc08be1b734ab2fae67b316e23f33cc17cbed40f
김정연 <jy.keem2468@gmail.com>
2023-08-18 16:28:24 +0900

    [ADD]검색 결과 데이터 상태 저장,업데이트할 searchSlice 생성

---
+++ src/redux/searchSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { StoreEntireInfo } from '@to1step/propose-backend';
+
+interface SearchState {
+  searchResults: StoreEntireInfo[]; // 검색 결과 데이터 저장
+}
+
+const initialState: SearchState = {
+  searchResults: [], // StoreEntireInfo 타입의 배열을 빈 배열로 초기화
+};
+
+const searchSlice = createSlice({
+  name: 'search', // 액션 타입 생성에 사용될 슬라이스 이름 설정
+  initialState,
+  reducers: {
+    setSearchResults: (state, action: PayloadAction<StoreEntireInfo[]>) => {
+      // 액션(PayloadAction<StoreEntireInfo[]>타입)을 처리해 상태 업데이트
+      state.searchResults = action.payload; // 검색 결과 데이터 상태 업데이트
+    },
+  },
+});
+
+export const searchActions = searchSlice.actions;
+export default searchSlice.reducer;
