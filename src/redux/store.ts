e331ae521bbd441ea38c4aa80adb39d1a5b24b20
김정연 <jy.keem2468@gmail.com>
2023-08-18 13:42:54 +0900

    [CHORE]백엔드의 Store 데이터  타입 redux/store.ts에 사용

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, Store } from 'redux';
 import locationReducer from './locationReducer';
+import { Store } from '@to1step/propose-backend';
 
 // store 설정 + reducer 합침
 // `locationReducer`합침
 export interface RootState {
   location: string; // location 상태 타입 확인
+  store: Store;
 }
 
 const rootReducer = combineReducers({
