c78e05b21ebe98c439990fc342adc077f0d68365
김정연 <jy.keem2468@gmail.com>
2023-08-31 15:47:23 +0900

    [CHORE]StoreCategory의 해당 숫자에 맞게 카테고리 출력

--- src/components/Post/Store/Category.tsx
+++ src/components/Post/Store/Category.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import { StoreCategory } from '@to1step/propose-backend/src/database/types/enums';
 
 interface CategoryProps {
-  category: number;
+  category: StoreCategory;
 }
 
 const Category = ({ category }: CategoryProps) => {
+  let categoryName = '';
+
+  switch (category) {
+    case StoreCategory.CAFE:
+      categoryName = '카페';
+      break;
+    case StoreCategory.RESTAURANT:
+      categoryName = '레스토랑';
+      break;
+    case StoreCategory.PARK:
+      categoryName = '공원';
+      break;
+    default:
+      categoryName = '알 수 없음';
+  }
   return (
     <div>
       <article>
-        <p className="text-xs border-black text-gray-700">{category}</p>
+        <p className="text-xs border-black text-gray-700">{categoryName}</p>
       </article>
     </div>
   );
