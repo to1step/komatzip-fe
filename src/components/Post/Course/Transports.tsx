e258c89735aa3e91c9929e8344ed67e32dc703c7
김정연 <jy.keem2468@gmail.com>
2023-08-31 15:54:02 +0900

    [CHORE]Transports의 해당 숫자에 맞게 교통수단 출력할 수 있게 수정

--- src/components/Post/Course/Transports.tsx
+++ src/components/Post/Course/Transports.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import { Transportation } from '@to1step/propose-backend/src/database/types/enums';
-import { Transport } from '@to1step/propose-backend';
 
 interface TransPortsProps {
-  transports: Transport[];
+  transports: Transportation;
 }
 
 const TransPorts = ({ transports }: TransPortsProps) => {
+  let transportName = '';
+
+  switch (transports) {
+    case Transportation.BUS:
+      transportName = '버스';
+      break;
+    case Transportation.SUBWAY:
+      transportName = '지하철';
+      break;
+    case Transportation.WALK:
+      transportName = '도보';
+      break;
+    default:
+    case Transportation.ELSE:
+      transportName = '기타';
+      break;
+  }
   return (
     <div>
-      {transports.map((index, transport) => (
-        <span key={index}>{Transportation[transport]}</span>
-      ))}
+      <article>
+        <p className="text-xs border-black text-gray-700">
+          교통수단 : {transportName}
+        </p>
+      </article>
     </div>
   );
 };
