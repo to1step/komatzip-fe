5b396d589ecdba11917cfd733c5310b18d3e68a4
김정연 <jy.keem2468@gmail.com>
2023-10-06 21:30:07 +0900

    [FIX]마이페이지를 들어갈 때 마다 out of memory라고 출력되는 문제 해결
    마이페이지의 NickName 컴포넌트가 무한 재귀호출되어서 생긴 문제

--- src/components/MyPage/NickName.tsx
+++ src/components/MyPage/NickName.tsx
@@ -2,9 +2,9 @@ const NickName = ({ nickname }: { nickname: string | null }) => {
   return (
     <section>
       <li>닉네임</li>
-      <li>
-        <NickName nickname={nickname} />
-      </li>
+      {/* <li>
+        <NickName nickname={nickname} /> // 재귀호출
+      </li> */}
       <li>
         <button className="underline">수정</button>
       </li>
