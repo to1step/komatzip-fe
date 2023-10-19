3621fa4e4fba4d74efe4ded836cac6f948c5596c
Hoseong On <ohs8283@naver.com>
2023-10-19 23:57:01 +0900

    [FEAT]  로그인 상태에 따른 사용자 닉네임 표시 기능 추가

--- src/components/MapModal/ReviewList.tsx
+++ src/components/MapModal/ReviewList.tsx
@@ -12,16 +12,14 @@ const ReviewList = ({ markerInfo, token }: ReviewListProps) => {
     null,
   );
   const [reviews, setReviews] = useState<StoreReview[]>([]);
-  const [reviewText, setReviewText] = useState<string>(''); // 리뷰 텍스트 입력 상태 변수
+  const [reviewText, setReviewText] = useState<string>('');
 
   useEffect(() => {
     if (markerInfo && prevMarkerInfo !== markerInfo) {
       console.log('markerInfo 데이터:', markerInfo);
       const fetchStoreInfo = async () => {
         try {
-          const response = await axios.get(
-            `https://api.to1step.shop/v1/stores/${markerInfo.uuid}`,
-          );
+          const response = await axios.get(`/v1/stores/${markerInfo.uuid}`);
           const storeInfo = response.data;
           console.log('서버 응답:', response);
           const storeReviews = storeInfo.storeReviews || [];
@@ -40,7 +38,7 @@ const ReviewList = ({ markerInfo, token }: ReviewListProps) => {
     console.log('리뷰 작성 버튼 클릭됨');
     try {
       const response = await axios.post(
-        `https://api.to1step.shop/v1/stores/${markerInfo.uuid}/review`,
+        `/v1/stores/${markerInfo.uuid}/review`,
         {
           text: reviewText,
         },
@@ -51,19 +49,18 @@ const ReviewList = ({ markerInfo, token }: ReviewListProps) => {
         },
       );
       console.log('서버 응답:', response);
-      const newReview = response.data as StoreReview; // 새로 작성된 리뷰 정보
+      const newReview = response.data as StoreReview;
       setReviews([...reviews, newReview]);
-      setReviewText(''); // 입력 폼 초기화
+      setReviewText('');
     } catch (error) {
-      console.error('리뷰 작성 오류:', error);
+      console.error('Error submitting review:', error);
     }
   };
 
-  // 리뷰 삭제 함수
   const handleReviewDelete = async (reviewUUID: string) => {
     try {
       await axios.delete(
-        `https://api.to1step.shop/v1/stores/${markerInfo?.uuid}/reviews/${reviewUUID}`,
+        `/v1/stores/${markerInfo?.uuid}/reviews/${reviewUUID}`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -77,24 +74,33 @@ const ReviewList = ({ markerInfo, token }: ReviewListProps) => {
   };
 
   return (
-    <div>
-      {/* 리뷰 작성 폼 */}
+    <div className="mt-4 flex">
+      <textarea
+        value={reviewText}
+        onChange={(e) => setReviewText(e.target.value)}
+        placeholder="리뷰를 작성하세요."
+        className="w-[500px] p-2 border rounded"
+      />
+      <button
+        onClick={handleReviewSubmit}
+        className="px-4 mt-1 h-[5vw] bg-blue-500 text-white rounded hover:bg-blue-600 ml-2"
+      >
+        작성
+      </button>
+
       <div className="mt-4">
-        <textarea
-          value={reviewText}
-          onChange={(e) => setReviewText(e.target.value)}
-          placeholder="리뷰를 작성하세요."
-          className="w-[400px]"
-        />
-        <button onClick={handleReviewSubmit}>작성</button>
+        {reviews.map((review) => (
+          <div key={review.uuid} className="border p-4 mb-2 rounded">
+            <p>{review.review}</p>
+            <button
+              onClick={() => handleReviewDelete(review.uuid)}
+              className="px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600 ml-2"
+            >
+              삭제
+            </button>
+          </div>
+        ))}
       </div>
-      {/* 리뷰 목록 출력 */}
-      {reviews.map((review) => (
-        <div key={review.uuid}>
-          {review.review}
-          <button onClick={() => handleReviewDelete(review.uuid)}>삭제</button>
-        </div>
-      ))}
     </div>
   );
 };
