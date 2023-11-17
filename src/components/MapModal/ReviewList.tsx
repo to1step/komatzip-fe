2fec53836116148dbd9ca8cebf3351fb26306a50
Hoseong On <ohs8283@naver.com>
2023-11-18 00:20:14 +0900

    [FEAT]리뷰 삭제시 자신의 리뷰만 삭제할수 있도록 변경

--- src/components/MapModal/ReviewList.tsx
+++ src/components/MapModal/ReviewList.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import axiosInstance from '../../api/apiInstance';
 import { StoreEntireInfo, StoreReview, Store } from '@to1step/propose-backend';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/module';
+import { success, errors } from '../../util/toastify';
 
 interface ReviewListProps {
   markerInfo: StoreEntireInfo | Store | null;
@@ -12,6 +15,8 @@ const ReviewList = ({ markerInfo }: ReviewListProps) => {
   );
   const [reviews, setReviews] = useState<StoreReview[]>([]);
   const [reviewText, setReviewText] = useState<string>('');
+
+  const userData = useSelector((state: RootState) => state.user.userData);
 
   useEffect(() => {
     if (markerInfo && prevMarkerInfo !== markerInfo) {
@@ -37,11 +42,12 @@ const ReviewList = ({ markerInfo }: ReviewListProps) => {
 
   const handleReviewSubmit = async () => {
     try {
-      if (markerInfo) {
+      if (markerInfo && userData) {
         const newReview = {
           uuid: Math.random().toString(),
           review: reviewText,
-          user: '현재 사용자 ID',
+          user: userData.nickname,
+          nickname: userData.nickname,
         };
 
         const response = await axiosInstance.post(
@@ -61,11 +67,25 @@ const ReviewList = ({ markerInfo }: ReviewListProps) => {
 
   const handleReviewDelete = async (reviewUUID: string) => {
     try {
-      if (markerInfo) {
-        await axiosInstance.delete(
-          `/v1/stores/${markerInfo.uuid}/reviews/${reviewUUID}`,
+      if (markerInfo && userData) {
+        const reviewToDelete = reviews.find(
+          (review) => review.uuid === reviewUUID,
         );
-        setReviews(reviews.filter((review) => review.uuid !== reviewUUID));
+        if (reviewToDelete) {
+          if (reviewToDelete.nickname === userData.nickname) {
+            await axiosInstance.delete(
+              `/v1/stores/${markerInfo.uuid}/reviews/${reviewUUID}`,
+            );
+            setReviews((prevReviews) =>
+              prevReviews.filter((review) => review.uuid !== reviewUUID),
+            );
+            success('리뷰가 삭제 되었습니다.');
+          } else {
+            errors('해당 리뷰를 삭제할 수 있는 권한이 없습니다.');
+          }
+        } else {
+          console.log('리뷰를 찾을 수 없습니다.');
+        }
       }
     } catch (error) {
       console.error('Error deleting review:', error);
