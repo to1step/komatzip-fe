47b90bbf7f54fd60c4136ab3dae82d48948bd065
Hoseong On <ohs66428283@gmail.com>
2023-12-28 14:59:03 +0900

    [CHORE]: apidusrruf

--- src/components/MapModal/LikeButton.tsx
+++ src/components/MapModal/LikeButton.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import axiosInstance from '../../api/apiInstance'; // axiosInstance를 가져옴
+import axiosInstance from '../../api/apiInstance';
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 import { Store, StoreEntireInfo } from '@to1step/propose-backend';
 
@@ -16,18 +16,12 @@ const LikeButton = ({ markerInfo }: LikeButtonProps) => {
 
   useEffect(() => {
     if (isClickLike) {
-      // 서버에 좋아요 정보 업데이트
       const saveUserLike = async () => {
         try {
           if (markerLike && markerInfo) {
-            // axiosInstance를 사용하여 API 요청을 처리
             await axiosInstance.post(`/v1/stores/${markerInfo.uuid}/like`);
           } else if (!markerLike && markerInfo) {
-            // axiosInstance를 사용하여 API 요청을 처리
-            await axiosInstance.delete(
-              `/v1/stores/${markerInfo.uuid}/like`,
-              {},
-            );
+            await axiosInstance.delete(`/v1/stores/${markerInfo.uuid}/like`);
           }
           localStorage.setItem(
             `like-${markerInfo.uuid}`,
